ddd7cea4fba2b782d9500f9140ae6502c4505035
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-09-17 22:15:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-center bg-red-500'>Vite + React</h1>
+      <button className="btn btn-accent mx-auto">Accent</button>
+      
+    </>
+  )
+}
+
+export default App
